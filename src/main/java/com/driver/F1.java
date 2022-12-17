84bdd4e9629c1229ed6c4ffcf1d85f6927a68785
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-17 16:57:20 +0530

    changes done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,19 +2,13 @@ package com.driver;
 
 public class F1 extends Car {
 
-
-    // constructor to initialize the variables
     public F1(String name, boolean isManual) {
+        //Use arbitrary values for parameters which are not mentioned
         super(name,isManual);
-
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0;
-        this.setCurrentSpeed(rate);
-
-        newSpeed=this.getCurrentSpeed();
-        //set the value of new speed by using currentSpeed and rate
+        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -24,27 +18,36 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        this.setCurrentSpeed(rate);
+
+        newSpeed=this.getCurrentSpeed();
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-            stop();
-            changeGear(1);
+            this.changeGear(1);
         }
 
         //for all other cases, change the gear accordingly
-        else if (newSpeed > 50 && newSpeed <= 100) {
-            changeGear(2);
-        } else if (newSpeed > 100 && newSpeed <= 150) {
-            changeGear(3);
-        } else if (newSpeed > 150 && newSpeed <= 200) {
-            changeGear(4);
-        } else if (newSpeed > 200 && newSpeed <= 250) {
-            changeGear(5);
-        } else if (newSpeed > 250) {
-            changeGear(6);
-    }
-        if(newSpeed > 0 ) {
+        if(newSpeed>0 && newSpeed<=50)
+            this.changeGear(1);
+        else if(newSpeed>50 && newSpeed<=100)
+            this.changeGear(2);
+        else if(newSpeed>100 && newSpeed<=150)
+            this.changeGear(3);
+        else if(newSpeed>150 && newSpeed<=200)
+            this.changeGear(4);
+        else if(newSpeed>200 && newSpeed<=250)
+            this.changeGear(5);
+        else if(newSpeed>250)
+            this.changeGear(6);
+
+
+
+        if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
-}
+
+    }
+
+
 }
