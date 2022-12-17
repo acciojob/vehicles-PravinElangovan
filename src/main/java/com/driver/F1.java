5daed659ddff7e61824158a36e8bbd18800eca03
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-17 14:29:46 +0530

    first commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,13 +1,20 @@
 package com.driver;
 
 public class F1 extends Car {
+    int currentSpeed;
+    // flag to indicate whether the F1 car is manual or not
+    boolean isManual;
 
+    // constructor to initialize the variables
     public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, "car", 4, 6, isManual, 1, 8);
+        this.currentSpeed = 0;
+        this.isManual = isManual;
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = currentSpeed + rate;
+        this.currentSpeed = newSpeed;//set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +27,28 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if (newSpeed > 50 && newSpeed <= 100) {
+            changeGear(2);
+        } else if (newSpeed > 100 && newSpeed <= 150) {
+            changeGear(3);
+        } else if (newSpeed > 150 && newSpeed <= 200) {
+            changeGear(4);
+        } else if (newSpeed > 200 && newSpeed <= 250) {
+            changeGear(5);
+        } else if (newSpeed > 250) {
+            changeGear(6);
 
-        if(newSpeed > 0) {
+        if(newSpeed > 0 ) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
+    }
+}
+
+    private int getCurrentDirection() {
+        return getCurrentDirection();
     }
 }
